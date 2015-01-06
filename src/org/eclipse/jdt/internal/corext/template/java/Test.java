6668a17962109584cb4e0a32a6363a62539cd07a
zhl2013 <zhanghongliang_cyj@163.com>
2015-01-06 16:09:00 +0800

    define-annotation-template

---
+++ src/org/eclipse/jdt/internal/corext/template/java/Test.java
@@ -0,0 +1,22 @@
+/*  
+ * @(#) Test.java Create on 2015-1-6 下午2:03:03   
+ *   
+ * Copyright 2015 .yhx
+ */
+
+
+package org.eclipse.jdt.internal.corext.template.java;
+
+/**
+ * @Test.java
+ * @created at 2015-1-6 下午2:03:03 by {253587517@qq.com}
+ *
+ * @desc
+ *
+ * @author  {253587517@qq.com}
+ * @version $Revision$
+ * @update: $Date$
+ */
+public class Test {
+
+}
