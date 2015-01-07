dce1de469a748bed096f816267eface0bd428f03
zhl2013 <zhanghongliang_cyj@163.com>
2015-01-07 10:56:55 +0800

    eclipse indigo luna

---
+++ src/org/eclipse/jdt/internal/corext/template/java/Test234.java
@@ -0,0 +1,25 @@
+/*  
+. * @(#) Test234.java Create on 2015年1月7日 上午10:31:19   
+ *   
+ * Copyright 2015 yhx.
+ */
+
+
+package org.eclipse.jdt.internal.corext.template.java;
+
+/**
+ * @Test234.java
+ * @created at 2015年1月7日 上午10:31:19 by {253587517@qq.com}
+ *
+ *
+ * @desc
+ *
+ * @author  {253587517@qq.com}
+ * @version $Revision$
+ * @update: $Date$
+ */
+public class Test234 {
+	public static void main(String[] args) {
+	}
+	
+}
